b53de0a7fa478deea67bd3d722165e6f193ad13e
harshapalnatiunh <HarshavardhanReddy.Palnati@unh.edu>
2024-02-20 15:30:09 -0800

    Add java template to the project

---
+++ website/src/main/java/com/deploy/website/DeployWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployWebsiteApplication.class, args);
+	}
+
+}
---
+++ website/src/test/java/com/deploy/website/DeployWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
